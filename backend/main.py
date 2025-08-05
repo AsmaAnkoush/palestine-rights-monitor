990d23547c93d99545e205e76420a0dd8474deeb
unknown <asmaa.ankoush2003@gmail.com>
2025-08-05 14:25:21 -0700

    Import final project including README

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "http://localhost:8006/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
